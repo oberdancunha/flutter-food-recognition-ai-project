b182b1dbc41ce687feb0daf75565f67dfa5f0494
oberdancunha <oberdancunha@gmail.com>
2023-08-18 11:00:55 -0300

    Install image_picker package.

--- flutter_food_recognition_dependency_module/lib/flutter_food_recognition_dependency_module.dart
+++ flutter_food_recognition_dependency_module/lib/flutter_food_recognition_dependency_module.dart
@@ -5,6 +5,7 @@ export 'package:connectivity_plus/connectivity_plus.dart';
 export 'package:dotenv/dotenv.dart';
 export 'package:equatable/equatable.dart';
 export 'package:flutter_modular/flutter_modular.dart';
+export 'package:image_picker/image_picker.dart';
 export 'package:kt_dart/kt.dart';
 export 'package:result_dart/result_dart.dart';
 export 'package:uno/uno.dart';
